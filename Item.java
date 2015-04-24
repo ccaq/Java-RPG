f6350bca4b71280b890f907b40b130d05df46188
jake41397 <jake41397@gmail.com>
2015-04-23 22:29:57 -0400

    items
    dfsdfd

--- Item.java
+++ Item.java
@@ -1,1 +1,34 @@
 //add code here jake
+package RPGC;
+
+public class Item {
+
+	String name;
+	int amount;
+	boolean haveIt;
+
+	public String toString() {
+		if (haveIt) {
+			if (amount > 0) {
+				return name + " x " + amount;
+			}
+			return name + "\n";
+		}
+		return "";
+	}
+
+	public Item(String name) {
+		this.name = name;
+		this.haveIt = false;
+	}
+
+	public void give(int amount) {
+		this.amount = amount;
+		this.haveIt = true;
+	}
+
+	public void remove() {
+		this.haveIt = false;
+	}
+
+}
