a88faa5a1b44bcec113174aa2e5d5d05255a3466
jake41397 <jake41397@gmail.com>
2015-04-23 22:27:56 -0400

    dfgdfg
    dfgdfg

--- Inventory.java
+++ Inventory.java
@@ -1,1 +1,19 @@
 //add code here jake
+package RPGC;
+
+import java.util.ArrayList;
+
+public class Inventory {
+StringBuilder sb = new StringBuilder();
+	
+	public String displayInv(ArrayList<Item> Inventory) {
+		sb.append("<html>");
+		for (int i = 0; i < Inventory.size(); i++) {
+			sb.append(Inventory.get(i) + "<br>");
+		}
+		sb.append("</html>");
+		System.out.println(sb);
+		return sb.toString();
+	}
+
+}
